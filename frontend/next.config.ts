4f89f7195717cce8ae187747b5350a06035feff4
Brandon Meyer <bmeyer24@gmail.com>
2025-05-29 01:07:09 +0000

    feat: enable standalone output for Next.js configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // Enables Next.js outputting a standalone folder
   /* config options here */
 };
 
